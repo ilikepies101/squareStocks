6b55d4ece3f29df3e2f39c58055e9e42d5363ad7
israelhilerio <32881015+ilikepies101@users.noreply.github.com>
2023-04-26 14:15:09 -0700

    update comment

--- app/src/test/java/com/example/stocks/StocksViewModelTest.kt
+++ app/src/test/java/com/example/stocks/StocksViewModelTest.kt
@@ -19,9 +19,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Unit tests for StocksViewModel
  */
 @RunWith(JUnit4::class)
 class StocksViewModelTest {
